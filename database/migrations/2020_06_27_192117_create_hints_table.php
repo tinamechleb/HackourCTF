5b126d5b3f8e2ff94035b3318f9696ee4d729efb
tinamechleb <tinamechleb@gmail.com>
2020-06-27 22:43:16 +0300

    added database migrations

---
+++ database/migrations/2020_06_27_192117_create_hints_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHintsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hints', function (Blueprint $table) {
+            $table->id();
+            $table->text("hint");
+            $table->integer("flag_id");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hints');
+    }
+}
